5f5eba394eb6e8cda73c6169bca0f0cd279618a8
sanha <kgpaper@naver.com>
2021-03-31 18:55:51 +0900

    21/03/31 studying...

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,18 @@
-import React from 'react'
+import { authService } from "fbase";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = ()=> <span>profile</span>
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
