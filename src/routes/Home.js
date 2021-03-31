d6f621a9f561d9eec181d42c93788173d05133a5
sanha <kgpaper@naver.com>
2021-04-01 00:45:15 +0900

    21/04/01 studying...

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,28 +1,23 @@
 import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [reweet, setReweet] = useState("");
   const [reweets, setReweets] = useState([]);
-  const getReweets = async () => {
-    const dbReweets = await dbService.collection("reweets").get();
-    dbReweets.forEach((document) => {
-      const reweetObject = {
-        ...document.data(),
-        id: document.id,
-      };
-      setReweets((prev) => [reweetObject, ...prev]);
-    });
-  };
+
   useEffect(() => {
-    getReweets();
+    dbService.collection("reweets").onSnapshot((snapshot) => {
+      const reweetArray = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setReweets(reweetArray);
+    });
   }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("reweets").add({
-      reweet,
+      text: reweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
     setReweet("");
   };
@@ -32,17 +27,19 @@ const Home = () => {
     } = event;
     setReweet(value);
   };
-  console.log(reweets);
+  
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={reweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="reweet" />
       </form>
-      <div key={reweet.id}>
-        {reweets.map(reweet => <div>
-          <h4>{reweet.reweet}</h4>
-        </div>)}
+      <div>
+        {reweets.map((reweet) => (
+          <div key={reweet.id}>
+            <h4>{reweet.text}</h4>
+          </div>
+        ))}
       </div>
     </div>
   );
