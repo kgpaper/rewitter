0394f6ae175745773bd89911574ca98850ff10dd
sanha <kgpaper@naver.com>
2021-04-01 00:22:07 +0900

    21/04/01 studying...

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [reweet, setReweet] = useState("");
+  const [reweets, setReweets] = useState([]);
+  const getReweets = async () => {
+    const dbReweets = await dbService.collection("reweets").get();
+    dbReweets.forEach((document) => {
+      const reweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setReweets((prev) => [reweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getReweets();
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -18,12 +32,18 @@ const Home = () => {
     } = event;
     setReweet(value);
   };
+  console.log(reweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={reweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="reweet" />
       </form>
+      <div key={reweet.id}>
+        {reweets.map(reweet => <div>
+          <h4>{reweet.reweet}</h4>
+        </div>)}
+      </div>
     </div>
   );
 };
