482bbfacdd134b615787e65e938a065a5428cef3
sanha <kgpaper@naver.com>
2021-04-01 00:05:24 +0900

    21/04/01 studying...

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-const Home = () => <span>home</span>
+const Home = () => {
+  const [reweet, setReweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("reweets").add({
+      reweet,
+      createdAt: Date.now(),
+    });
+    setReweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setReweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={reweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="reweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
