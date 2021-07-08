b25a5bf10db8f4474bd4d66c07421e64cbe65797
sanha <kgpaper@naver.com>
2021-07-09 03:33:00 +0900

    21/07/09 studying...

--- src/components/App.js
+++ src/components/App.js
@@ -8,19 +8,31 @@ function App() {
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        if(user.displayName==null){
-          const ind = user.email.indexOf("@")
-          const end = user.email.substring(0,ind)
-          user.updateProfile({displayName:end})
+        if (user.displayName == null) {
+          const ind = user.email.indexOf("@");
+          const end = user.email.substring(0, ind);
+          user.updateProfile({ displayName: end });
         }
-        setUserObj(user)
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
   return (
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing"}
+      {init ? <AppRouter refreshUser={refreshUser} isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing"}
       <footer>&copy; Rewitter {new Date().getFullYear()} </footer>
     </>
   );
